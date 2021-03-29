b30e74da1e2e3a570cd0e3c9c79545da98ffcd2e
daiking <daikynguyen999@gmail.com>
2021-03-29 23:33:53 +0900

    update ui

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //jshint esversion:6
 
-const express = require("express");//Install express framework
-const bodyParser = require("body-parser"); //Parse data in body request
-const ejs = require("ejs"); //Import ejs
-const _ = require("lodash");
-const PORT = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,58 +11,70 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-app.set('view engine', 'ejs');//Set view mode by ejs
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-let posts =[];
 
-app.get("/", function(req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
   });
 });
 
-app.get("/about", function(req, res) {
-  res.render("about", {startingContent: aboutContent
-  });
-  
-});
-
-app.get("/contact", function(req, res) {
-  res.render("contact", {startingContent: contactContent});
-});
-
-app.get("/compose", function(req, res) {
+app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res) {
-  const post = {
+app.post("/compose", function(req, res){
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
 });
 
-app.get("/posts/:postName", function(req, res) {
-  console.log(req.params.postName)
-  const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach(function(post) {
-    const storedTitle = _.lowerCase(post.title);
+app.get("/posts/:postId", function(req, res){
 
-    if (storedTitle == requestedTitle) {
-      res.render("post", {title: post.title, content: post.content})
-    } else {
+const requestedPostId = req.params.postId;
 
-    }
-  })
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
 
 
-})
-
-app.listen(PORT, function() {
-  console.log("Server started on port ", PORT);
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
